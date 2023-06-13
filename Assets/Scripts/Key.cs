8b8a6cd65dfad2b57baf3120bda51388b3ac61bc
enyafunke <50293129+enyafunke@users.noreply.github.com>
2023-06-13 23:51:22 +0200

    Add clearing scene and more packages

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Door"))
+            OpenDoor();
+
+    }
+
+    private void OpenDoor()
+    {
+        Debug.Log("open door");
+    }
+}
