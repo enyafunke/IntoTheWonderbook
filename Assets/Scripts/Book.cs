f2632b699bd8e8d67073a31a89d4eeb15988fb6e
LacunaPatio <julia.lueckhof@study.hs-duesseldorf.de>
2023-07-05 16:15:47 +0200

    added Tutorial Scene

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -6,6 +6,7 @@ public class Book : MonoBehaviour
 {
     [SerializeField] GameObject items;
     bool key, drink, eat;
+    public GameObject rabbit;
 
 
     private void OnTriggerEnter(Collider other)
@@ -27,6 +28,21 @@ public class Book : MonoBehaviour
             {
                 drink = true;
             }
+            if (other.gameObject.CompareTag("Rabbit"))
+            {
+                other.gameObject.transform.localScale = new Vector3(.2f, .2f, .2f);
+                other.gameObject.transform.parent = items.transform;
+                other.gameObject.transform.localPosition = new Vector3(0, 0, -0.1f);
+                other.gameObject.transform.eulerAngles = new Vector3(-70f, 0f, 180f);
+            }
         }
     }
+
+    /*private void Start()
+    {
+        rabbit.transform.localScale = new Vector3(.2f, .2f, .2f);
+        rabbit.transform.parent = items.transform;
+        rabbit.transform.localPosition = new Vector3(0, 0, -0.1f);
+        rabbit.transform.eulerAngles = new Vector3(-70f, 0f, 180f);
+    }*/
 }
---
+++ Assets/Scripts/Rabbit.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR;
+
+public class Rabbit : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject transitionPlane;
+    bool startTransition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            StartCoroutine(ToWonderland());
+        }
+    }
+
+    IEnumerator ToWonderland()
+    {
+        //anim.SetTrigger("transition");
+        transitionPlane.SetActive(true);
+        startTransition = true;
+
+        yield return new WaitForSeconds(5);
+
+        SceneManager.LoadScene(1);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ToWonderland());
+    }
+
+    private void Update()
+    {
+        if(startTransition)
+        {
+            Color color = transitionPlane.GetComponent<Material>().color;
+
+            if (color.a < 255)
+            {
+                color.a++;
+                transitionPlane.GetComponent<Material>().color = color;
+            }
+        }
+    }
+}
