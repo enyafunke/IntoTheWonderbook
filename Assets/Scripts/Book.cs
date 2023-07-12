369dd6ce40f550d2f3d5fdce507fad3ce116ac14
enyaf <enyafunke@gmail.com>
2023-07-12 15:27:17 +0200

    book line

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Book : MonoBehaviour
 {
     [SerializeField] GameObject items;
+    [SerializeField] GameObject line;
     bool key, drink, eat;
     public GameObject rabbit;
 
@@ -30,6 +31,7 @@ public class Book : MonoBehaviour
             }
             if (other.gameObject.CompareTag("Rabbit"))
             {
+                line.SetActive(false);
                 other.gameObject.transform.localScale = new Vector3(.2f, .2f, .2f);
                 other.gameObject.transform.parent = items.transform;
                 other.gameObject.transform.localPosition = new Vector3(0, 0, -0.1f);
