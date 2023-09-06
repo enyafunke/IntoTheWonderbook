0a5852b9146d3c7978f6f1c9b8ce93a637e070a5
YannickHSD <105860199+YannickHSD@users.noreply.github.com>
2023-09-06 15:56:43 +0200

    fix line

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,9 +13,10 @@ public class Line : MonoBehaviour
     void Update()
     {
         /*lineRenderer.SetPosition(0, book.transform.position);
-        lineRenderer.SetPosition(1, rabbit.transform.position);*/
+        lineRenderer.SetPosition(1, rabbit.transform.position);
+        */
         lineRenderer.positionCount = 2;
-        Vector3[] pos = { book.transform.position, rabbit.transform.position };
+        Vector3[] pos = {book.transform.position, rabbit.transform.position};
         lineRenderer.SetPositions(pos);
         /*if (Vector3.Distance(book.transform.position, rabbit.transform.position) < 3)
         {
